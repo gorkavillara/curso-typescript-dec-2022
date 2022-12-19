a3f9efc2836dab1e70f052e6790526a4768027fc
gorkavillar <gorka@gorkavillar.com>
2022-12-19 10:26:47 +0100

    🛠️ Configuración inicial

---
+++ src/controllers/alumnosController.ts
@@ -0,0 +1,4 @@
+export const alumno = {
+    nombre: "Ricardo",
+    edad: 19
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { alumno } from "./controllers/alumnosController"
+// Ctrl + ç => comentar/descomentar
+
+console.log("Hola!")
+// console.log("Nombre del alumno: " + alumno.nombre + ", edad: " + alumno.edad)
+console.log(`Nombre del alumno: ${alumno.nombre}, edad: ${alumno.edad}`)
