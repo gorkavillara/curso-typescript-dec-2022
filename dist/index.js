fc752cdfdb0ae6ff953cefe43319ee344bb803b5
gorkavillar <gorka@gorkavillar.com>
2022-12-20 12:43:10 +0100

    🖱️ Primer evento de click

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
 console.log("Eventos con typescript");
+// const button: HTMLElement | null = document.getElementById("click-button")
+// const button = document.getElementById("click-button")! as HTMLButtonElement
+const button = document.querySelector("button#click-button");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", (event) => {
+    console.log(event);
+    alert("Has hecho click");
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 console.log("Eventos con typescript")
+
+// const button: HTMLElement | null = document.getElementById("click-button")
+// const button = document.getElementById("click-button")! as HTMLButtonElement
+const button: HTMLButtonElement | null = document.querySelector("button#click-button")
+
+button?.addEventListener("click", (event: MouseEvent) => {
+    console.log(event)
+    alert("Has hecho click")
+})
