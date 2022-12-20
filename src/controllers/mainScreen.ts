78bd54f3fd6b1b1a86c9c5da22000d5cca31c949
gorkavillar <gorka@gorkavillar.com>
2022-12-20 13:34:00 +0100

    💬 Conversación con el usuario

--- src/controllers/mainScreen.ts
+++ src/controllers/mainScreen.ts
@@ -1,32 +1,40 @@
-import chalk from "chalk"
-import inquirer from "inquirer"
+import chalk from "chalk";
+import inquirer, { PromptModule, Answers } from "inquirer";
+import { choices } from "../assets/index.js";
 
 export const mainScreen = async (): Promise<void> => {
-    let loop: boolean = true
-    // Dar la bienvenida
-    sayHello()
-    // Preguntar el nombre
-    const username = await getUserName()
-    while (loop) {
-        // Mostrar las opciones
-        loop = showOptions()
-    }
-}
+  let loop: boolean = true;
+  // Dar la bienvenida
+  sayHello();
+  // Preguntar el nombre
+  const username = await getUserName();
+  while (loop) {
+    // Mostrar las opciones
+    loop = await showOptions();
+  }
+};
 
 const sayHello = (): void => {
-    console.log(chalk.bgCyan("Bienvenid@ al CRM con Typescript"))
-}
+  console.log(chalk.bgCyan("Bienvenid@ al CRM con Typescript"));
+};
 
 const getUserName = async (): Promise<string> => {
-    const question = await inquirer.prompt({
-        message: "Cuál es tu nombre",
-        name: "user_name",
-        type: "input",
-        default: "Super Mario"
-    })
-    return question.user_name
-}
+  const question = await inquirer.prompt({
+    message: "Cuál es tu nombre",
+    name: "user_name",
+    type: "input",
+    default: "Super Mario",
+  });
+  return question.user_name;
+};
 
-const showOptions = (): boolean => {
-    return false
-}
+const showOptions = async (): Promise<boolean> => {
+  const options = await inquirer.prompt({
+    message: "Qué te gustaría hacer",
+    name: "option_selected",
+    type: "list",
+    choices,
+  });
+  if (options.option_selected === "e. SALIR") return false
+  return true
+};
