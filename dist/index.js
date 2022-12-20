b8463aec3ee70e8f704a31124a2271d0cbc98eb8
gorkavillar <gorka@gorkavillar.com>
2022-12-20 12:55:02 +0100

    🥳 Tema 7 - Eventos finalizado

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,12 @@ button === null || button === void 0 ? void 0 : button.addEventListener("click",
     console.log(event);
     alert("Has hecho click");
 });
+const form = document.querySelector("form");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+});
+const nameInput = document.querySelector("input[name=nombre]");
+nameInput.addEventListener("input", (event) => console.log(event));
+// nameInput.oninput = (event) => console.log(event)
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,15 @@ button?.addEventListener("click", (event: MouseEvent) => {
     console.log(event)
     alert("Has hecho click")
 })
+
+const form: HTMLFormElement | null = document.querySelector("form")
+
+form?.addEventListener("submit", (event: SubmitEvent) => {
+    event.preventDefault()
+    console.log(event)
+})
+
+const nameInput: HTMLInputElement = document.querySelector("input[name=nombre]")!
+
+nameInput.addEventListener("input", (event) => console.log(event))
+// nameInput.oninput = (event) => console.log(event)
