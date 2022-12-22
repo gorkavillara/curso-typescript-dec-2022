29ebd154ab6e9ec35a165d5a969ad0c90518a127
gorkavillar <gorka@gorkavillar.com>
2022-12-22 09:51:43 +0100

    🍪 Utilizando las cookies como herramienta de persistencia en el navegador

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,53 @@
 "use strict";
-console.log("Eventos con typescript");
-// const button: HTMLElement | null = document.getElementById("click-button")
-// const button = document.getElementById("click-button")! as HTMLButtonElement
-const button = document.querySelector("button#click-button");
-button === null || button === void 0 ? void 0 : button.addEventListener("click", (event) => {
-    console.log(event);
-    alert("Has hecho click");
+// 1 - Si el usuario no está logueado -> mostramos (por defecto)
+// 2 - Si el usuario se loguea -> lo eliminamos y lanzamos un saludo
+const form = document.querySelector("form#login-form");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("form#login-form > input[name=nombre]");
+    // const username: string | null = document.querySelector("form#login-form > input[name=nombre]")?.value
+    const username = input ? input.value : null;
+    if (!username)
+        return;
+    storeName(username);
+    sayHello(username);
 });
-const form = document.querySelector("form");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log(event);
-});
-const nameInput = document.querySelector("input[name=nombre]");
-nameInput.addEventListener("input", (event) => console.log(event));
-// nameInput.oninput = (event) => console.log(event)
+const sayHello = (name) => {
+    if (!form)
+        return;
+    // Oculta el formulario
+    form.style.display = "none";
+    // Muestre el saludo
+    const formDiv = document.querySelector("div.formulario");
+    if (!formDiv)
+        return;
+    const helloSpan = document.createElement('span');
+    helloSpan.innerText = `Hola ${name}`;
+    helloSpan.classList.add("hello");
+    formDiv.appendChild(helloSpan);
+};
+const storeName = (username) => {
+    document.cookie = `username=${username}`;
+};
+const checkUserLoggedIn = () => {
+    // Chequear si el usuario está logueado
+    const username = getUsernameFromCookies();
+    if (!username)
+        return;
+    // Si está logueado -> Escondemos el formulario + Lanzamos un saludo
+    sayHello(username);
+};
+const getUsernameFromCookies = () => {
+    const cookies = document.cookie;
+    const cookiesArray = cookies.split(";");
+    const userNameCookie = cookiesArray.find((cookie) => {
+        const cookieName = cookie.split("=")[0];
+        if (cookieName.trim() === "username")
+            return true;
+    });
+    if (userNameCookie)
+        return userNameCookie.split("=")[1];
+    return null;
+};
+checkUserLoggedIn();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,56 @@
-console.log("Eventos con typescript")
+// 1 - Si el usuario no está logueado -> mostramos (por defecto)
+// 2 - Si el usuario se loguea -> lo eliminamos y lanzamos un saludo
 
-// const button: HTMLElement | null = document.getElementById("click-button")
-// const button = document.getElementById("click-button")! as HTMLButtonElement
-const button: HTMLButtonElement | null = document.querySelector("button#click-button")
+const form: HTMLFormElement | null = document.querySelector("form#login-form")
 
-button?.addEventListener("click", (event: MouseEvent) => {
-    console.log(event)
-    alert("Has hecho click")
+form?.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const input: HTMLInputElement | null = document.querySelector("form#login-form > input[name=nombre]")
+    // const username: string | null = document.querySelector("form#login-form > input[name=nombre]")?.value
+    const username: string | null = input ? input.value : null
+    if (!username) return
+
+    storeName(username)
+    sayHello(username)
 })
 
-const form: HTMLFormElement | null = document.querySelector("form")
+const sayHello = (name: string): void => {
+    if (!form) return
 
-form?.addEventListener("submit", (event: SubmitEvent) => {
-    event.preventDefault()
-    console.log(event)
-})
+    // Oculta el formulario
+    form.style.display = "none"
 
-const nameInput: HTMLInputElement = document.querySelector("input[name=nombre]")!
+    // Muestre el saludo
+    const formDiv: HTMLDivElement | null = document.querySelector("div.formulario")
+    if (!formDiv) return
+    
+    const helloSpan: HTMLSpanElement = document.createElement('span')
+    helloSpan.innerText = `Hola ${name}`
+    helloSpan.classList.add("hello")
+    formDiv.appendChild(helloSpan)
+}
 
-nameInput.addEventListener("input", (event) => console.log(event))
-// nameInput.oninput = (event) => console.log(event)
+const storeName = (username: string): void => {
+    document.cookie = `username=${username}`
+}
+
+type CookiesType = string
+const checkUserLoggedIn = (): void => {
+    // Chequear si el usuario está logueado
+    const username = getUsernameFromCookies()
+    if (!username) return
+    // Si está logueado -> Escondemos el formulario + Lanzamos un saludo
+    sayHello(username)
+}
+const getUsernameFromCookies = (): string | null => {
+    const cookies: CookiesType = document.cookie
+    const cookiesArray: string[] = cookies.split(";")
+    const userNameCookie: string | undefined = cookiesArray.find((cookie: string) => {
+        const cookieName = cookie.split("=")[0]
+        if (cookieName.trim() === "username") return true
+    })
+    if (userNameCookie) return userNameCookie.split("=")[1]
+    return null
+}
+
+checkUserLoggedIn()
