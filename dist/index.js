be504eb5696de43715473121da6c7f72b290a534
gorkavillar <gorka@gorkavillar.com>
2022-12-22 12:49:52 +0100

    🧑🏻‍🎓 Clases en Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,53 +1,34 @@
 "use strict";
-// 1 - Si el usuario no está logueado -> mostramos (por defecto)
-// 2 - Si el usuario se loguea -> lo eliminamos y lanzamos un saludo
-const form = document.querySelector("form#login-form");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const input = document.querySelector("form#login-form > input[name=nombre]");
-    // const username: string | null = document.querySelector("form#login-form > input[name=nombre]")?.value
-    const username = input ? input.value : null;
-    if (!username)
-        return;
-    storeName(username);
-    sayHello(username);
-});
-const sayHello = (name) => {
-    if (!form)
-        return;
-    // Oculta el formulario
-    form.style.display = "none";
-    // Muestre el saludo
-    const formDiv = document.querySelector("div.formulario");
-    if (!formDiv)
-        return;
-    const helloSpan = document.createElement('span');
-    helloSpan.innerText = `Hola ${name}`;
-    helloSpan.classList.add("hello");
-    formDiv.appendChild(helloSpan);
-};
-const storeName = (username) => {
-    document.cookie = `username=${username}`;
-};
-const checkUserLoggedIn = () => {
-    // Chequear si el usuario está logueado
-    const username = getUsernameFromCookies();
-    if (!username)
-        return;
-    // Si está logueado -> Escondemos el formulario + Lanzamos un saludo
-    sayHello(username);
-};
-const getUsernameFromCookies = () => {
-    const cookies = document.cookie;
-    const cookiesArray = cookies.split(";");
-    const userNameCookie = cookiesArray.find((cookie) => {
-        const cookieName = cookie.split("=")[0];
-        if (cookieName.trim() === "username")
-            return true;
-    });
-    if (userNameCookie)
-        return userNameCookie.split("=")[1];
-    return null;
-};
-checkUserLoggedIn();
+class Student {
+    constructor(n, a, fl, isDev) {
+        // Inicializamos los atributos de este objeto
+        this.name = n;
+        this.age = a;
+        if (fl)
+            this.favoriteLanguage = fl;
+        if (isDev)
+            this.isDeveloper = isDev;
+    }
+    getFavoriteLanguage() {
+        this.logFavoriteLanguage();
+        return this.favoriteLanguage;
+    }
+    logFavoriteLanguage() {
+        console.log(this.favoriteLanguage);
+    }
+    get nameAndAge() {
+        return `${this.name}, is ${this.age} years old`;
+    }
+    set newFavoriteLanguage(nfl) {
+        this.favoriteLanguage = nfl;
+        this.isDeveloper = true;
+    }
+}
+const newStudent = new Student("Ismael", 22, "Typescript", false); // Instanciar una clase
+newStudent.newFavoriteLanguage = "Vue3";
+// console.log(newStudent.nameAndAge())
+console.log(newStudent.nameAndAge);
+// newStudent.newFavoriteLanguage("Vue3")
+console.log(newStudent.isDeveloper);
+console.log(newStudent.getFavoriteLanguage());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,41 @@
-// 1 - Si el usuario no está logueado -> mostramos (por defecto)
-// 2 - Si el usuario se loguea -> lo eliminamos y lanzamos un saludo
+class Student { // Creación de una clase
+    name: string
+    age: number
+    private favoriteLanguage?: string
+    isDeveloper?: boolean
 
-const form: HTMLFormElement | null = document.querySelector("form#login-form")
+    constructor(n: string, a: number, fl?: string, isDev?: boolean) {
+        // Inicializamos los atributos de este objeto
+        this.name = n
+        this.age = a
+        if (fl) this.favoriteLanguage = fl
+        if (isDev) this.isDeveloper = isDev
+    }
 
-form?.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const input: HTMLInputElement | null = document.querySelector("form#login-form > input[name=nombre]")
-    // const username: string | null = document.querySelector("form#login-form > input[name=nombre]")?.value
-    const username: string | null = input ? input.value : null
-    if (!username) return
+    getFavoriteLanguage() {
+        this.logFavoriteLanguage()
+        return this.favoriteLanguage
+    }
 
-    storeName(username)
-    sayHello(username)
-})
+    private logFavoriteLanguage() {
+        console.log(this.favoriteLanguage)
+    }
 
-const sayHello = (name: string): void => {
-    if (!form) return
+    get nameAndAge() {
+        return `${this.name}, is ${this.age} years old`
+    }
 
-    // Oculta el formulario
-    form.style.display = "none"
-
-    // Muestre el saludo
-    const formDiv: HTMLDivElement | null = document.querySelector("div.formulario")
-    if (!formDiv) return
-    
-    const helloSpan: HTMLSpanElement = document.createElement('span')
-    helloSpan.innerText = `Hola ${name}`
-    helloSpan.classList.add("hello")
-    formDiv.appendChild(helloSpan)
+    set newFavoriteLanguage(nfl: string) {
+        this.favoriteLanguage = nfl
+        this.isDeveloper = true
+    }
 }
 
-const storeName = (username: string): void => {
-    document.cookie = `username=${username}`
-}
+const newStudent: Student = new Student("Ismael", 22, "Typescript", false) // Instanciar una clase
+newStudent.newFavoriteLanguage = "Vue3"
+// console.log(newStudent.nameAndAge())
+console.log(newStudent.nameAndAge)
 
-type CookiesType = string
-const checkUserLoggedIn = (): void => {
-    // Chequear si el usuario está logueado
-    const username = getUsernameFromCookies()
-    if (!username) return
-    // Si está logueado -> Escondemos el formulario + Lanzamos un saludo
-    sayHello(username)
-}
-const getUsernameFromCookies = (): string | null => {
-    const cookies: CookiesType = document.cookie
-    const cookiesArray: string[] = cookies.split(";")
-    const userNameCookie: string | undefined = cookiesArray.find((cookie: string) => {
-        const cookieName = cookie.split("=")[0]
-        if (cookieName.trim() === "username") return true
-    })
-    if (userNameCookie) return userNameCookie.split("=")[1]
-    return null
-}
-
-checkUserLoggedIn()
+// newStudent.newFavoriteLanguage("Vue3")
+console.log(newStudent.isDeveloper)
+console.log(newStudent.getFavoriteLanguage())
