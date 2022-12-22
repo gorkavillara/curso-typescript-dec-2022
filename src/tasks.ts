53e28d0ade72e68ff315168f431fb8597184143c
gorkavillar <gorka@gorkavillar.com>
2022-12-22 11:11:50 +0100

    🖋️ localStorage y sessionStorage aprendidos

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,19 +1,68 @@
-const tasksForm: HTMLFormElement | null = document.querySelector("form#tasks-form")
-const tasksList: HTMLUListElement | null = document.querySelector("ul#tasks-list")
+interface Task {
+  text: string;
+  done: boolean;
+}
+
+const tasksForm: HTMLFormElement | null =
+  document.querySelector("form#tasks-form");
+const tasksList: HTMLUListElement | null =
+  document.querySelector("ul#tasks-list");
+const clearListButton: HTMLButtonElement | null = document.querySelector("button#clear-list")
 
 tasksForm?.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const newTask: HTMLInputElement | null = document.querySelector("input[name=newtask]")
-    
-    if (!newTask) return
+  const newTask: HTMLInputElement | null = document.querySelector(
+    "input[name=newtask]"
+  );
 
-    const newLi = document.createElement('li')
-    newLi.innerText = newTask.value
-    tasksList?.appendChild(newLi)
+  if (!newTask) return;
+  if (newTask.value === "") return;
+  // Crea una nueva tarea
+  const task: Task = getNewTask(newTask.value)
+  // Almacena esta nueva tarea en el local Storage
+  storeNewTask(task)
+  newTask.value = "";
+renderList()
+});
 
-    newTask.value = ""
-})
+const getNewTask = (taskText: string): Task => ({
+  text: taskText,
+  done: false,
+});
 
-// Cada vez que haya una entrada -> Pintamos appendChild un nuevo li
-// Que contenga la información del input
+const storeNewTask = (newTask: Task): void => {
+    const localStorageTasks: string | null = localStorage.getItem("tasks")
+    if (!localStorageTasks) {
+        const newLocalStorageTasks = [newTask]
+        return localStorage.setItem("tasks", JSON.stringify(newLocalStorageTasks))
+    }
+    const newLocalStorageTasks = [...JSON.parse(localStorageTasks), newTask]
+    return localStorage.setItem("tasks", JSON.stringify(newLocalStorageTasks))
+}
+
+const renderList = (): void => {
+    const localStorageTasks: string | null = localStorage.getItem("tasks")
+    if (!localStorageTasks) return
+
+    const taskList: Task[] = JSON.parse(localStorageTasks)
+    if (tasksList) tasksList.innerHTML = ""
+    taskList.forEach((task: Task) => {
+        const newLi: HTMLLIElement = document.createElement('li')
+        newLi.innerText = task.text
+        tasksList?.appendChild(newLi)
+    })
+}
+const clearUl = (): void => {
+    if (tasksList) tasksList.innerHTML = ""
+}
+
+const clearList = (): void => {
+    // Eliminar el item "tasks" del localStorage
+    localStorage.removeItem("tasks")
+    // Renderizar de nuevo la lista
+    clearUl()
+}
+clearListButton?.addEventListener("click", clearList)
+
+renderList()
